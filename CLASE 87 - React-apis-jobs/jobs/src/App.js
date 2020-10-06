0a0f1eb16e3c963a4cf0ccebbcd73cb57aec9fb8
Ornella <ornee.sci@gmail.com>
2020-10-06 00:15:04 -0300

    react

---
+++ CLASE 87 - React-apis-jobs/jobs/src/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import './App.scss';
+import Table from './Componentes/Table/Table';
+import axios from 'axios';
+
+function App() {
+
+  const [users, setUsers] = useState([]);
+  const[jobs, setJobs] = useState([]);
+
+  const getPost = async () =>{
+    try{
+      const res = await axios.get('https://5f518d325e98480016123ada.mockapi.io/api/v1/users');
+      const res2 = await axios.get('https://5f518d325e98480016123ada.mockapi.io/api/v1/jobs');
+      setUsers(res.data)
+      setJobs(res2.data)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    getPost();
+  },[]);
+
+  return (
+      <div className="container">
+        <table className="table-jobs">
+        <thead className="table-head">
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Avatar</th>
+            <th>Job</th>
+            <th>Cambiar job</th>
+        </thead>
+        <tbody className="table-body">
+          {
+            users.map(user=>{
+              const userJob = jobs.find(job => job.id == user.jobId);
+              if(!userJob) {
+                console.log(user);
+                return null;
+              }
+              return (<Table 
+                id={user.id} 
+                name={user.name} 
+                avatar={user.avatar}
+                job={userJob.name}
+              >
+              <td>
+                <select className="drop">
+                  <option hidden>Elegir job</option>
+                  {jobs.map(job =>{
+                    return (
+                        <option 
+                          value={job.id}
+                          selected={job.id == userJob.id ? 'selected' : ''}
+                        >{job.name}</option>
+                      )
+                    })
+                  }
+                </select>
+              </td>
+              </Table>)
+            })
+          }       
+        </tbody>
+      </table>
+      </div>
+  
+  );
+}
+//TODO 29-9 hacer el ONCHANGE del select y asi modificar el job :)
+
+
+
+
+export default App;
